c4a5555a81f6b3b9a691eb57bf2f39c152183960
Misha <sherstoun001@gmail.com>
2022-06-27 15:44:29 +0300

    application optimization react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./scss/app.scss";
 
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import { Cart } from "./pages/Cart";
+/* import { Cart } from "./pages/Cart"; */
 import { FullPizza } from "./pages/FullPizza";
 
-function App() {
+const Cart = React.lazy(() => import("./pages/Cart"));
 
+function App() {
   return (
     <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="*" element={<NotFound />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/pizza/:id" element={<FullPizza/>} />
-            </Routes>
-          </div>
+      <Header />
+      <div className="content">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route
+              path="/cart"
+              element={
+                <Suspense fallback={<div>Загрузка...</div>}>
+                  <Cart />
+                </Suspense>
+              }
+            />
+            <Route path="/pizza/:id" element={<FullPizza />} />
+          </Routes>
         </div>
+      </div>
     </div>
   );
 }
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -6,7 +6,7 @@ import { clearItems } from "../redux/cart/slise";
 import { CartEmpty } from "../components/CartEmty";
 import { selectCart } from "../redux/cart/selectors";
 
-export const Cart: React.FC = () => {
+const Cart: React.FC = () => {
   const dispatch = useDispatch();
   const { totalPrice, items } = useSelector(selectCart);
 
@@ -148,3 +148,5 @@ export const Cart: React.FC = () => {
     </div>
   );
 };
+
+export default Cart;
